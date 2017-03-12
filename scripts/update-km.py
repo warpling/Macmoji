8cbeec7e4c1515d6ab9e2b91c3d5c8ca95fc6a88
valrus <imccowan@gmail.com>
2017-03-12 11:14:17 -0700

    Add some Python idioms, switch to plistlib

--- scripts/update-km.py
+++ scripts/update-km.py
@@ -1,25 +1,25 @@
-import xml.etree.ElementTree
 import io
 import json
-import os, sys
+import os
+import plistlib
+import sys
 
-# Gets the script's directory so we can access files relative to it
-scripts_directory = os.path.dirname(os.path.realpath(__file__))
 
-settings_file_dir = os.path.join(scripts_directory, 'settings.json')
-with open(settings_file_dir) as settings_file:
-    settings = json.load(settings_file)
+def main():
+    # Gets the script's directory so we can access files relative to it
+    scripts_directory = os.path.dirname(os.path.realpath(__file__))
 
-# Load in the canonical emoji substitutions
-emoji_substitutions_file = os.path.join(scripts_directory, '../emoji substitutions.plist')
-e = xml.etree.ElementTree.parse(emoji_substitutions_file).getroot()
+    settings_file_dir = os.path.join(scripts_directory, 'settings.json')
+    with open(settings_file_dir) as settings_file:
+        settings = json.load(settings_file)
 
-formatted_data = {}
-for idx, value in enumerate(e.findall('./array/dict/string')):
-    if idx % 2 == 0:
-        emoji = value.text
-    else:
-        placeholder = value.text
+    # Load in the canonical emoji substitutions
+    emoji_substitutions_file = os.path.join(scripts_directory, '../emoji substitutions.plist')
+    emoji_substitutions = plistlib.readPlist(emoji_substitutions_file)
+
+    formatted_data = {}
+    for item in emoji_substitutions:
+        emoji, placeholder = item['phrase'], item['shortcut']
         for setting in settings:
             if setting['name'] in formatted_data:
                 formatted_data[setting['name']] += setting['data_format'].format(
@@ -28,10 +28,15 @@ for idx, value in enumerate(e.findall('./array/dict/string')):
                 formatted_data[setting['name']] = setting['data_format'].format(
                     placeholder=placeholder, emoji=emoji)
 
-# Fill out each template
-for settings in settings:
-    template_file = os.path.join(scripts_directory, '../templates/' + setting['name'] + '.template')
-    with io.open(template_file, 'r', encoding='utf8') as s:
-        template = s.read()
-    with io.open('../' + setting['name'], 'w', encoding='utf8') as f:
-        f.write(template.format(formatted_data[setting['name']]))
+    # Fill out each template
+    for settings in settings:
+        template_file = os.path.join(scripts_directory,
+                                     '../templates/' + setting['name'] + '.template')
+        with io.open(template_file, 'r', encoding='utf8') as s:
+            template = s.read()
+        with io.open('../' + setting['name'], 'w', encoding='utf8') as f:
+            f.write(template.format(formatted_data[setting['name']]))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
